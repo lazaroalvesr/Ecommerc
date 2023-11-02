6e9327706b0a45f53e56ea395b10fe5dd981d217
lazaro <lazaroalves12355@gmail.com>
2023-11-02 18:03:54 -0300

    feat: conectando_api

---
+++ src/app/components/ProductImage.tsx
@@ -0,0 +1,34 @@
+'use client';
+import Image from "next/image";
+import { useState } from 'react';
+import { ProductType } from "@/types/ProductType";
+
+type ProductImage = {
+    product: ProductType;
+    fill?: boolean;
+}
+
+export default function ProductImage({ product, fill }: ProductImage) {
+    const [loading, setLoading] = useState(true);
+
+    return fill ? (
+        <Image
+            src={product.image}
+            fill
+            alt={product.title}
+            className={`object-cover 
+            ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`}
+            onLoadingComplete={() => setLoading(false)}
+        />
+    ) : (
+        <Image
+            src={product.image}
+            width={400}
+            height={700}
+            alt={product.title}
+            className={`object-cover 
+            ${loading ? 'scale-110 blur-3xl grayscale' : 'scale-100 blur-0 grayscale-0'}`}
+            onLoadingComplete={() => setLoading(false)}
+        />
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,30 @@
+import { ProductType } from "@/types/ProductType";
+import Product from "./components/Product";
 
-export default function Home() {
+async function getProducts() {
+  const res = await fetch('https://fakestoreapi.com/products')
+
+  if (!res.ok) {
+
+    throw new Error('falhou')
+  }
+  return res.json()
+}
+
+
+export default async function Home() {
+  const products = await getProducts();
+
   return (
-    <div>
-      <h1>Ecommerce</h1>
+    <div className="max-w-5xl mx-auto pt-8 
+    px-8 xl:px-0">
+      <div className="grid grid-cols-1 
+      sm:grid-cols-2 md:grid-cols-3 
+      lg:grid-cols-4 gap-10 xl:gap-6">
+          {products.map((product: ProductType) => (
+           <Product key={product.id} product={product}></Product>
+          ))}
+      </div>
     </div>
   )
 }
